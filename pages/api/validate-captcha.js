4d972b307964a7efe1875cee0c63769abb1a37a0
TheoEkoha <olivieri.theo@gmail.com>
2025-03-13 20:04:04 +0100

    cpatcha ok

---
+++ pages/api/validate-captcha.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+      const { token } = req.body;
+  
+      // Valider le token avec l'API de reCAPTCHA
+      const response = await fetch(
+        `https://www.google.com/recaptcha/api/siteverify?secret=6LfMsvIqAAAAAH2h4sqJcbBP4-RTHPQhcI8m-zar&response=${token}`,
+        {
+          method: 'POST',
+        }
+      );
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        res.status(200).json({ success: true });
+      } else {
+        console.error('Erreurs reCAPTCHA :', data['error-codes']);
+        res.status(400).json({ success: false, error: 'Captcha invalide', errorCodes: data});
+      }
+    } else {
+      res.status(405).json({ error: 'Méthode non autorisée' });
+    }
+  }
